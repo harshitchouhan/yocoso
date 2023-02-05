d1ecc3499b99e434517fc5bb8520be07b9e29a79
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2023-02-05 19:45:12 +0530

    Routes Management table created

--- src/app/components/route-management/route-management.component.ts
+++ src/app/components/route-management/route-management.component.ts
@@ -1,15 +1,776 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from "@angular/core";
+import { Observable } from "rxjs";
+import { NgbdSortableHeader, SortEvent } from "src/app/shared/directives/NgbdSortableHeader";
+import { TableService } from "./route-management.service";
 
 @Component({
-  selector: 'app-route-management',
-  templateUrl: './route-management.component.html',
-  styleUrls: ['./route-management.component.scss']
+  selector: "app-route-management",
+  templateUrl: "./route-management.component.html",
+  styleUrls: ["./route-management.component.scss"],
 })
 export class RouteManagementComponent implements OnInit {
+  public tableItem$: Observable<any[]>;
+  total$: Observable<number>;
 
-  constructor() { }
+  public defaultBindingsList = [
+    { value: "10", label: "10" },
+    { value: "20", label: "20" },
+    { value: "30", label: "30" },
+    { value: "50", label: "50" },
+  ];
 
-  ngOnInit(): void {
+  selectedEntries = { value: "20", label: "20" };
+
+  constructor(public service: TableService) {
+    this._fetchCustomers();
+    this.tableItem$ = service.tableItem$;
+    this.total$ = service.total$;
   }
 
+  ngOnInit() {}
+
+  @ViewChildren(NgbdSortableHeader) headers!: QueryList<NgbdSortableHeader>;
+
+  onSort({ column, direction }: SortEvent) {
+    // resetting other headers
+    this.headers.forEach((header) => {
+      if (header.sortable !== column) {
+        header.direction = "";
+      }
+    });
+
+    this.service.sortColumn = column;
+    this.service.sortDirection = direction;
+  }
+
+  handleTextChange(e: any) {
+    this.service.searchTerm = e.target.value;
+  }
+
+  private _fetchCustomers = () => {
+    let data = [
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Pali",
+        postalCodes: ["123", "456", "789"],
+        status: "active",
+      },
+      {
+        id: "123",
+        title: "Jodhpur",
+        postalCodes: ["123456", "456789", "789123"],
+        status: "active",
+      },
+    ];
+
+    this.service.initialData = data;
+  };
 }
